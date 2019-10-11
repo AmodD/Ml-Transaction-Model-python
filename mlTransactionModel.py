ae28b47a502ca218777e9b75d14c24d10dabed42
diwakar <diwakar@fortiate.com>
2019-10-11 18:06:35 +0530

    TV-82 #comment Cleaned the data, splitted POS_DATA, Done train test split

--- mlTransactionModel.py
+++ mlTransactionModel.py
@@ -1,62 +1,195 @@
+import re
 import pandas as pd
 import numpy as np
-#import os.path
+import os.path
 import pathlib
-file = "/home/diwakar/Downloads/Sample Transactions-test Data.csv"
-features=["PROCESSING_CODE","POS_DATA","Transaction Amount","Merchant Category Code","POS Entry Mode","Response Code"]
+from numpy.ma import column_stack
+
+# for supressing warnings
+import warnings
+
+warnings.filterwarnings("ignore")
+
+# giving the file path and the required features
+data_csv_path = "Sample Transactions with Target Variable.csv"
+features = ["PROCESSING_CODE", "POS_DATA", "TRANSACTION_AMOUNT", "POS_ENTRY_MODE", "CARD_ACCEPTOR_ACTIVITY",
+            "CODE_ACTION"]
 features.sort()
 print(features)
-length=len(features)
+length = len(features)
+abc = pathlib.Path(data_csv_path)
+read_csv_variable = pd.read_csv
 
 
-abc= pathlib.Path(file)
-print(abc.exists())
-def importFile(file):
+# importing csv file
+def importdata_csv_path(data_csv_path):
     if abc.exists():
-        # df = pd.read_csv(file, dtype=object)
-        df=pd.read_csv(file, dtype={'POS_DATA': 'str'})
-        print(df.head(2600))
-        return df
+        csv_data = read_csv_variable(data_csv_path, dtype=object)
+        return csv_data
     else:
-        raise FileNotFoundError("File not found")
-    #print(df)
-    return file
+        raise FileNotFoundError("data_csv_path not found")
+    return data_csv_path
 
-def featureExtraction(file,fearures,length):
-    if len(features)==length:
-        if(all(x in list(df) for x in features)):
-            df1=file[features]
-            #print(features)
-            # df1[features[2]] = df1[features[2]].apply(lambda x: x.zfill(12))
-            # df[features[1]] = df1[features[1]].replace('???', 0)
-            # #df1.to_csv("test.csv",header=True)
-            return list(df1)
+
+# checking the required features are present in the csv file
+def featureExtraction(data_frame, fearures, length):
+    if len(features) == length:
+        if all(x in list(data_frame) for x in features):
+            data_frame = data_frame[features]
+            return list(data_frame)
         else:
             raise NameError("Features not found")
     else:
         raise IndexError("Length not matching")
 
-    #print(df1)
+
+# calling the import method and it returns a dataframe
+data = importdata_csv_path(abc)
+
+# passing that dataframe for checking the features
+feature = featureExtraction(data, features, length)
+# print(feature)
+# if the features are present it will return a list of features that will be passed as an argument for the dataframe
+# for selecting that particular features alone
+data_with_req_features = data[feature]
+
+# making the POS_DATA column as 12 digits using zfill fn
+# data_with_req_features[features[2]] = data_with_req_features[features[2]].str.zfill(12)
+
+# replacing the ??? in the POS Entry Mode to '000' finding the POS_ENTRY_MODE by index
+index = feature.index('POS_ENTRY_MODE')
+data_with_req_features[features[index]] = data_with_req_features[features[index]].replace('???', '000')
+
+# finding the columns having Exponential values
+# having_E = data_with_req_features[features[2]].str.contains('E')
+
+# filtering ot those columns with not operator
+# dataframe = data_with_req_features[~having_E]
+
+# assigning appropriate types for all the columns
+
+convert_dict = {'POS_ENTRY_MODE': str,
+                'POS_DATA': str,
+                'PROCESSING_CODE': str,
+                'TRANSACTION_AMOUNT': float,
+                'CARD_ACCEPTOR_ACTIVITY': str,
+                'CODE_ACTION': str
+                }
+
+dataframe = data_with_req_features.astype(convert_dict)
+print(dataframe.dtypes)
+
+index = list(dataframe).index('POS_DATA')
+
+# splitting the POS_DATA column into 12 different columns
+dataframe['TERM_CARD_READ_CAP'] = dataframe['POS_DATA'].str[0:1]
+dataframe['TERM_CH_VERI_CAP'] = dataframe['POS_DATA'].str[1:2]
+dataframe['TERM_CARD_CAPTURE_CAP'] = dataframe['POS_DATA'].str[2:3]
+dataframe['TERM_ATTEND_CAP'] = dataframe['POS_DATA'].str[3:4]
+dataframe['CH_PRESENCE_IND'] = dataframe['POS_DATA'].str[4:5]
+dataframe['CARD_PRESENCE_IND'] = dataframe['POS_DATA'].str[5:6]
+dataframe['TXN_CARD_READ_IND'] = dataframe['POS_DATA'].str[6:7]
+dataframe['TXN_CH_VERI_IND'] = dataframe['POS_DATA'].str[7:8]
+dataframe['TXN_CARD_VERI_IND'] = dataframe['POS_DATA'].str[8:9]
+dataframe['TRACK_REWRITE_CAP'] = dataframe['POS_DATA'].str[9:10]
+dataframe['TERM_OUTPUT_IND'] = dataframe['POS_DATA'].str[10:11]
+dataframe['PIN_ENTRY_IND'] = dataframe['POS_DATA'].str[11:12]
+
+# deleting the POS_DATA column after splitting
+dataframe.drop(columns=['POS_DATA'], inplace=True)
+
+dataframe.to_csv('task.csv')
+
+# print(new[0])
+# print(new)
+# print(dataframe.iloc[70445])
+print(dataframe.dtypes)
+# = dataframe[feature[index]]
+# print(a)
+
+# removing indexes
+# dataframe.reset_index()
+
+# for time being deleting the POS_DATA column for training and creating a model
+# dataframe.drop(columns=['POS_DATA'], inplace=True)
+
+# selecting X and y for the training data, X is the inputs and y is the response variable
+X = dataframe.iloc[:, [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]]
+y = dataframe.iloc[:, 1]
+print(X)
+print(y)
+
+# importing required packages for train test split, confusion matrix,accuracy score and for KNN
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.metrics import classification_report
+from sklearn.neighbors import KNeighborsClassifier
+#
+# # from sklearn.model_selection import  GridSearchCV
+# # parameters={'n_neighbours':np.array([1,3,5,7,9])}
+# # knn=KNeighborsClassifier()
+# # cv=GridSearchCV(knn,param_grid=parameters,cv=5)
+# # cv.fit(X,y)
+# # print(cv.cv_results_)
+# # print(cv.best_params_)
+# # print(cv.best_score_)
+# # print(cv.best_estimator_)
+#
+# splitting the training and testing data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2019,stratify=y)
+
+# applying KNN classification algorithm (as our response variable is response code, categorical data) with nearest
+# neighbours = 3
+try:
+    knn = KNeighborsClassifier(n_neighbors=5)
+    knn.fit(X_train, y_train)
+    y_pred = knn.predict(X_test)
+    # print confusion matrix and classification report
+    print(confusion_matrix(y_test, y_pred))
+    print(classification_report(y_test, y_pred))
+
+    # printing accuracy score
+    print(accuracy_score(y_test, y_pred))
+
+except ValueError as e:
+  print(e)
 
 
-df = importFile(abc)
-feature=featureExtraction(df,features,length)
-df2=df[feature]
-print(df2.iloc[2600])
-# df2[features[2]] = df2[features[2]].apply(lambda x: x.zfill(12))
-# df2[features[1]] = df2[features[1]].replace('???', 0)
-# df2[features[1]] = df2[features[1]].apply(lambda x: x.zfill(3))
 
-df2[features[2]] = df2[features[2]].str.zfill(12)
-df2[features[1]] = df2[features[1]].replace('???', '000')
-#df2[features[1]] = df2[features[2]].replace('????????????', '000000000000')
-#df2[features[1]] = df2[features[2]].replace('????????????', '000000000000')
-print(df2.dtypes)
-#df2[feature[2]].astype(float)
-#df2[feature[2]]=pd.to_numeric(df2[feature[2]])
-print(df2)
-# print(df2.dtypes)
-# print(df2.loc[200])
+
+# from sklearn.metrics import roc_curve, roc_auc_score
+#
+# y_pred_proba = knn.predict_proba(X_test)[:, 3]
+# fpr, tpr, treshold = roc_curve(y_test, y_pred_proba)
+
+# testdf=pd.read_csv("/home/diwakar/FORTIATE/BUILD/WORKSPACES/PYTHON/Ml-Transaction-Model/test.csv",dtype=object,index_col=False)
+# dum_df=(testdf.iloc[:,1:])
+# print(dum_df.head())
+# print(dum_df.dtypes)
+# dum_df = dum_df.astype({'Merchant Category Code':'object','POS Entry Mode':'object','POS_DATA':'str','PROCESSING_CODE':'object','Response Code':'object','Transaction Amount':'double'})
+# print(dum_df.dtypes)
+# # dum_df=dum_df['POS_DATA'].filter(regex='!+')
+#
+# dum_df['POS_DATA'] = dum_df['POS_DATA'].astype('str')
+# print(dum_df['POS_DATA'].dtypes)
+# dum_df['POS_DATA'].str.split(expand=True)
+# print(dum_df['POS_DATA'])
+# # print(dum_df.iloc[20])
+#
+# # dum_df.POS_DATA.str.extractall('', flags=re.U)[0].unstack().rename_axis(None, 1)
+#
+# # dum_df['POS_DATA'].apply(lambda x: pd.Series(list(x)))
+#
+# 1
+# 2
+
+# df3 = pd.DataFrame(dum_df.POS_DATA.str.split().tolist().split())
+# print(df3)
+
+# dum_df=dum_df.dropna(dum_df['POS_DATA'])
+# dum_df = dum_df[~dum_df['POS_DATA'].str.contains('E')]
+# print(data_with_req_features.dtypes)
+# print(data_with_req_features.loc[200])
 # print(all(x in list(df) for x in features))
 # if list(df).__contains__(features):
 #     print("Yes")
@@ -64,30 +197,43 @@ print(df2)
 #     print("No")
 
 
-
-
-#print(df)
+# print(df)
 # print(list(df))
 # df=df[features]
 # print(list(df))
-#print(df1)
+# print(df1)
 # df1['POS_DATA'] = df1['POS_DATA'].apply(lambda x: x.zfill(12))
-#df1['POS_DATA']=df1['POS_DATA'].apply(lambda x: '{0:0>12}'.format(x))
+# df1['POS_DATA']=df1['POS_DATA'].apply(lambda x: '{0:0>12}'.format(x))
 
 # #df1['POS Entry Mode']=df1['POS Entry Mode'].fillna(value=0)
 # #df1.loc[df1['POS Entry Mode'].isnull(),'POS Entry Mode']=0
-#df1['POS Entry Mode'] = df1['POS Entry Mode'].replace('???', 0)
-# df2=pd.read_csv("/home/diwakar/FORTIATE/BUILD/WORKSPACES/PYTHON/Ml-Transaction-Model/file2.csv",dtype=object)
-# print(df2.head(4000))
-# print(df2.loc[3256])
-
+# df1['POS Entry Mode'] = df1['POS Entry Mode'].replace('???', 0)
+# data_with_req_features=pd.read_csv("/home/diwakar/FORTIATE/BUILD/WORKSPACES/PYTHON/Ml-Transaction-Model/data_csv_path2.csv",dtype=object)
+# print(data_with_req_features.head(4000))
+# print(data_with_req_features.loc[3256])
 
 
 # from datetime import datetime
 # start = datetime.now()
-# dataframe=pd.read_csv(file,dtype=object)
+# dataframe=pd.read_csv(data_csv_path,dtype=object)
 # end = datetime.now()
 # time_taken = end - start
 # print('Time: ',time_taken)
 
 
+# print(data_with_req_features.iloc[2600])
+# data_with_req_features[features[2]] = data_with_req_features[features[2]].apply(lambda x: x.zfill(12))
+# data_with_req_features[features[1]] = data_with_req_features[features[1]].replace('???', '000')
+# data_with_req_features[features[1]] = data_with_req_features[features[1]].apply(lambda x: x.zfill(3))
+
+
+# print(data_with_req_features.iloc[2600])
+# data_with_req_features[features[1]] = data_with_req_features[features[2]].replace('????????????', '000000000000')
+# data_with_req_features[features[1]] = data_with_req_features[features[2]].replace('????????????', '000000000000')
+# print(data_with_req_features.dtypes)
+# data_with_req_features[feature[2]].astype(float)
+# data_with_req_features[feature[2]]=pd.to_numeric(data_with_req_features[feature[2]])
+# print(data_with_req_features)
+
+
+# new=dataframe['POS_DATA'].str.split(r',\d*', expand=True)
--- test_mlTransactionModel.py
+++ test_mlTransactionModel.py
@@ -3,69 +3,67 @@ import pandas as pd
 import string
 import unittest
 import mlTransactionModel
+read_csv_variable=pd.read_csv
+data_csv_path = "Sample Transactions with Target Variable.csv"
+data_csv_path1 = "Sample Transactions-Test Data-Assignment-2.csv"
+
+data = read_csv_variable(data_csv_path, dtype=object)
+length = len(data)
+data2 = pd.read_csv("test.csv")
+length1 = len(list(data2))
 
 
-path="/home/diwakar/Downloads/Sample Transactions-test Data.csv"
-path1="/home/diwakar/Downloads/Sample Transactions-Test Data-Assignment-2.csv"
-#path3="/home/diwakar/Downloads/Sample Transactions-test Data.txt"
-df1=pd.read_csv(path,dtype=object)
-b=len(df1)
-df=pd.read_csv("/home/diwakar/FORTIATE/BUILD/WORKSPACES/PYTHON/Ml-Transaction-Model/test.csv")
-a=len(list(df))
+def test_csv_format():
+    try:
+        with open(data_csv_path, newline='') as csvfile:
+            start = csvfile.read(4096)
+
+            # isprintable does not allow newlines, printable does not allow umlauts...
+            if not all([c in string.printable or c.isprintable() for c in start]):
+                return False
+            dialect = csv.Sniffer().sniff(start)
+            return True
+    except csv.Error:
+        print("Exception")
+        # Could not get a csv dialect -> probably not a csv.
+        return False
+
 
 class MyTestCase(unittest.TestCase):
-    # def test_csv(self):
-    #     self.assertEqual(mlTransactionModel.importFile("/home/diwakar/Downloads/Sample Transactions-test Data.csv"),path )
-    # def test_csv_not_found(self):
-    #     with self.assertRaises(FileNotFoundError):
-    #         mlTransactionModel.importFile(path1)
-    #
-    # def test_csv_format(self):
-    #     try:
-    #         with open(path, newline='') as csvfile:
-    #             start = csvfile.read(4096)
-    #
-    #             # isprintable does not allow newlines, printable does not allow umlauts...
-    #             if not all([c in string.printable or c.isprintable() for c in start]):
-    #                 return False
-    #             dialect = csv.Sniffer().sniff(start)
-    #             return True
-    #     except csv.Error:
-    #         print("Exception")
-    #         # Could not get a csv dialect -> probably not a csv.
-    #         return False
-    # def test_format(self):
-    #     self.assertEqual(path.__contains__(".csv"),True)
+    def test_csv(self):
+        self.assertEqual(mlTransactionModel.importFile("Sample Transactions-test Data.csv"),
+                         data_csv_path)
 
-    def test_feature(self):
-        with self.assertRaises(NameError):
-            mlTransactionModel.featureExtraction(df,list(df),a)
+    def test_csv_not_found(self):
+        with self.assertRaises(FileNotFoundError):
+            mlTransactionModel.importFile(data_csv_path1)
 
-    def test_feature_wrong(self):
-        with self.assertRaises(NameError):
-            mlTransactionModel.featureExtraction(df1,list(df1),b)
+    def test_format(self):
+        self.assertEqual(data_csv_path.__contains__(".csv"), True)
 
-
+    # def test_feature_wrong(self):
+    #     with self.assertRaises(NameError):
+    #         mlTransactionModel.featureExtraction(data, list(data2), length)
 
     def test_feature_len(self):
         with self.assertRaises(IndexError):
-            mlTransactionModel.featureExtraction(df,list(df),9)
-
+            mlTransactionModel.featureExtraction(data, list(data), 9)
 
     def test_feature_success(self):
-        self.assertEqual(mlTransactionModel.featureExtraction(df,list(df),a),list(df))
-
+        self.assertEqual(mlTransactionModel.featureExtraction(data, list(data), length), list(data))
 
     # def test_completeTest(self):
-    #     pathCSV = path
+    #     pathCSV = data_csv_path
     #
-    #     with open(path) as csvfile:
+    #     with open(data_csv_path) as csvfile:
     #         reader = csv.reader(csvfile)
     #         for row in reader:
     #             for item in row:
     #                 try:
-    #                     getattr(mlTransactionModel.importFile(path), item)()
+    #                     getattr(mlTransactionModel.importFile(data_csv_path), item)()
     #                 except AttributeError:
     #                     print("Unknown attribute", item, "ignored")
+
+
 if __name__ == '__main__':
     unittest.main()
